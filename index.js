189656f81968a2420a7e13c02df0c813e9952ec9
Anton Kozyr <anton.kozyr@globallogic.com>
2019-11-05 14:18:05 +0200

    Add bull and express packages to the dependencies

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello from queue manager');
+const express = require('express')
+
+const PORT = 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(PORT, () => console.log(`Queue manager listening on port ${PORT}!`))
